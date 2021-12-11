b517c17028785fb53f40a08e19cdd88a94b24876
chhzh123 <hc676@cornell.edu>
2021-12-11 18:49:10 -0500

    [pass] Refactor & Add loop names & Add comments

--- lib/passes/loop_tiling.cpp
+++ lib/passes/loop_tiling.cpp
@@ -22,25 +22,31 @@ struct HCLLoopTiling : public PassWrapper<HCLLoopTiling, FunctionPass> {
   StringRef getDescription() const final {
     return "Loop tiling in HeteroCL";
   }
+
+  void runSplitting();
+  void runTiling();
 };
 
 } // namespace
 
-template <typename RangeT>
-static auto findArg(RangeT &&range, StringRef name) {
-  auto it = llvm::find_if(range, [=](auto &arg) { return arg.first.str() == name; });
-  return it != range.end() ? &*it : nullptr;
-}
-
-// https://github.com/llvm/llvm-project/blob/release/13.x/mlir/lib/Dialect/Affine/Transforms/LoopTiling.cpp
-void HCLLoopTiling::runOnFunction()  {
-  // TODO: Support multiple splits/tiles
+/*
+ *  Algorithm:
+ *  1) Iterate schedule and get all SplitOp
+ *  2) For each SplitOp, find corresponding loop
+ *  3) Add names to new loops
+ *  4) Remove the schedule operator
+ */
+void HCLLoopTiling::runSplitting() {
   FuncOp f = getFunction();
   SmallVector<AffineForOp, 6> tiledNest;
   for (hcl::SplitOp splitOp : f.getOps<hcl::SplitOp>()) {
+    // 1) get schedule
     unsigned int factor = splitOp.factor();
     const auto loop_name = splitOp.loop().getType().cast<hcl::LoopHandleType>().getLoopName();
-    f.walk([&](AffineForOp forOp) { // loop nest traversal
+
+    // 2) Traverse all the nested loops and find the requested one
+    //    and split the loop
+    f.walk([&](AffineForOp forOp) {
       // mlir/IR/Operation.h
       Attribute attr = forOp->getAttr("loop_name");
       if (loop_name == attr.cast<StringAttr>().getValue()) {
@@ -52,10 +58,11 @@ void HCLLoopTiling::runOnFunction()  {
           return signalPassFailure();
       }
     });
+
+    // 3) Add names to new loops
     bool outLoopFlag = true;
     for (AffineForOp newForOp : tiledNest) {
-      // llvm::raw_ostream &output = llvm::outs();
-      // newForOp->print(output);
+      // TODO: check whether tiledNest only has two loops
       std::string new_name = loop_name.str();
       if (outLoopFlag) {
         outLoopFlag = false;
@@ -65,9 +72,16 @@ void HCLLoopTiling::runOnFunction()  {
       }
       newForOp->setAttr("loop_name", StringAttr::get(newForOp->getContext(), new_name));
     }
+
+    // 4) Remove the schedule operator
     splitOp.erase();
   }
+}
+
+void HCLLoopTiling::runTiling() {
+  FuncOp f = getFunction();
   for (hcl::TileOp tileOp : f.getOps<hcl::TileOp>()) {
+    // 1) get schedule
     unsigned int x_factor = tileOp.x_factor();
     unsigned int y_factor = tileOp.y_factor();
     const StringRef x_loop = tileOp.x_loop().getType().cast<hcl::LoopHandleType>().getLoopName();
@@ -76,20 +90,23 @@ void HCLLoopTiling::runOnFunction()  {
     SmallVector<unsigned, 6> tileSizes;
     tileSizes.push_back(x_factor);
     tileSizes.push_back(y_factor);
+
+    // 2) Traverse all the nested loops and find the requested one
+    //    and do tiling
     bool isFound = false;
-    f.walk([&](AffineForOp forOp) { // loop nest traversal
+    f.walk([&](AffineForOp forOp) {
       if (isFound)
         return;
-      const NamedAttribute* attr = findArg(forOp->getAttrs(), "loop_name");
-      const StringRef curr_loop_name = attr->second.cast<StringAttr>().getValue();
+      Attribute attr = forOp->getAttr("loop_name");
+      const StringRef curr_loop_name = attr.cast<StringAttr>().getValue();
       if (x_loop == curr_loop_name) {
         // LoopUtils.cpp getPerfectlyNestedLoopsImpl
         forOps.clear();
         AffineForOp rootForOp = forOp;
         for (unsigned i = 0; i < 2; ++i) {
-          if (i == 2) {
-            const NamedAttribute* attr_y = findArg(rootForOp->getAttrs(), "loop_name");
-            const StringRef curr_y_loop = attr_y->second.cast<StringAttr>().getValue();
+          if (i == 1) {
+            Attribute attr_y = rootForOp->getAttr("loop_name");
+            const StringRef curr_y_loop = attr_y.cast<StringAttr>().getValue();
             if (curr_y_loop != y_loop)
               break;
           }
@@ -111,10 +128,35 @@ void HCLLoopTiling::runOnFunction()  {
     SmallVector<AffineForOp, 6> tiledNest;
     if (failed(tilePerfectlyNested(forOps, tileSizes, &tiledNest)))
       return signalPassFailure();
-    tileOp.erase();
-  }
-}
 
+    // 3) Add names to new loops
+    unsigned cnt_loop = 0;
+    for (AffineForOp newForOp : tiledNest) {
+      // TODO: check whether tiledNest only has four loops
+      std::string new_name;
+      if (cnt_loop < 2)
+        new_name = x_loop.str();
+      else
+        new_name = y_loop.str();
+      if (cnt_loop % 2 == 0)
+        new_name += ".outer";
+      else
+        new_name += ".inner";
+      newForOp->setAttr("loop_name", StringAttr::get(newForOp->getContext(), new_name));
+      cnt_loop++;
+    }
+  }
+  // 4) Remove the schedule operator
+  // TODO: may have !NodePtr->isKnownSentinel() bug
+  // for (hcl::TileOp tileOp : f.getOps<hcl::TileOp>())
+  //   tileOp.erase();
+}
+// https://github.com/llvm/llvm-project/blob/release/13.x/mlir/lib/Dialect/Affine/Transforms/LoopTiling.cpp
+void HCLLoopTiling::runOnFunction()  {
+  // TODO: Support multiple splits/tiles
+  runSplitting();
+  runTiling();
+}
 
 namespace mlir {
 namespace hcl{

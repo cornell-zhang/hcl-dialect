0fb41cbfafbee7cdd170f55f84ccd33c83747a40
chhzh123 <hc676@cornell.edu>
2022-01-07 23:23:28 -0500

    [Binding] Add Affine dialect

---
+++ include/hcl/Bindings/Python/affine/_affine_ops_ext.py
@@ -0,0 +1,75 @@
+#  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+#  See https://llvm.org/LICENSE.txt for license information.
+#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+try:
+  from mlir.ir import *
+except ImportError as e:
+  raise RuntimeError("Error loading imports from extension module") from e
+
+from typing import Any, Sequence
+
+
+class AffineForOp:
+  """Specialization for the Affine for op class."""
+
+  def __init__(self,
+               lower_bound,
+               upper_bound,
+               step,
+               lowerBoundMap,
+               upperBoundMap,
+               iter_args: Sequence[Any] = [],
+               name="",
+               *,
+               loc=None,
+               ip=None):
+    """Creates an Affine `for` operation.
+    operation   ::= `affine.for` ssa-id `=` lower-bound `to` upper-bound
+                    (`step` integer-literal)? `{` op* `}`
+
+    lower-bound ::= `max`? affine-map-attribute dim-and-symbol-use-list | shorthand-bound
+    upper-bound ::= `min`? affine-map-attribute dim-and-symbol-use-list | shorthand-bound
+    shorthand-bound ::= ssa-id | `-`? integer-literal
+
+    - `lower_bound` is the value to use as lower bound of the loop.
+    - `upper_bound` is the value to use as upper bound of the loop.
+    - `step` is the value to use as loop step.
+    - `iter_args` is a list of additional loop-carried arguments.
+    """
+    results = [arg.type for arg in iter_args]
+    attributes = {}
+    attributes["step"] = step
+    attributes["lower_bound"] = lowerBoundMap
+    attributes["upper_bound"] = upperBoundMap
+    attributes["loop_name"] = name
+    if lower_bound == None and upper_bound == None:
+        operands = list(iter_args)
+    else:
+        operands = [lower_bound, upper_bound] + list(iter_args)
+    super().__init__(
+        self.build_generic(
+            regions=1,
+            results=results,
+            operands=operands,
+            attributes=attributes,
+            loc=loc,
+            ip=ip))
+    self.regions[0].blocks.append(IndexType.get(), *results)
+
+  @property
+  def body(self):
+    """Returns the body (block) of the loop."""
+    return self.regions[0].blocks[0]
+
+  @property
+  def induction_variable(self):
+    """Returns the induction variable of the loop."""
+    return self.body.arguments[0]
+
+  @property
+  def inner_iter_args(self):
+    """Returns the loop-carried arguments usable within the loop.
+    To obtain the loop-carried operands, use `iter_args`.
+    """
+    return self.body.arguments[1:]

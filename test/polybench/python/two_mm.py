0636a595e2469305a212a68c37eb900c59b0ab2d
Niansong Zhang <nz264@cornell.edu>
2022-01-20 14:10:29 -0500

    [Test] clean up code

--- test/polybench/python/two_mm.py
+++ test/polybench/python/two_mm.py
@@ -40,38 +40,18 @@ def top_2mm(
     s3 = hcl.create_schedule([A, B, C, D], kernel_2mm)
 
     #### Applying customizations ####
+    AB = kernel_2mm.out_AB
+    ABC = kernel_2mm.out_ABC
+    E = kernel_2mm.E
 
-    # AB = kernel_2mm.out_AB
-    # ABC = kernel_2mm.out_ABC
-    # E = kernel_2mm.E
+    s1[AB].compute_at(s1[ABC], ABC.axis[0])
+    s1[ABC].compute_at(s1[E], E.axis[0])
 
-    # ## N Buggy 1
-    # s1[AB].compute_at(s1[ABC], ABC.axis[0])
-    # s1[ABC].compute_at(s1[E], E.axis[0])
+    s2[AB].compute_at(s2[ABC], ABC.axis[0])
+    s2[ABC].compute_at(s2[E], E.axis[1])
 
-    # ## N Buggy 2
-    # s2[AB].compute_at(s2[ABC], ABC.axis[0])
-    # s2[ABC].compute_at(s2[E], E.axis[1])
-
-    # ## N Buggy 3
-    # s3[E].reorder(E.axis[1], E.axis[0])
-    # s3[AB].compute_at(s3[ABC], ABC.axis[0])
-
-    ## Buggy 1
-    # s[AB].compute_at(s[ABC], ABC.axis[1])
-    # s[ABC].compute_at(s[D], D.axis[0])
-
-    ## Buggy 2
-    # s[AB].compute_at(s[ABC], ABC.axis[1])
-    # s[ABC].compute_at(s[D], D.axis[1])
-
-    ## Buggy 3
-    # s[D].reorder(D.axis[1], D.axis[0])
-    # s[AB].compute_at(s[ABC], ABC.axis[1])
-
-    # s[D].parallel(D.axis[1])
-
-    #### Applying customizations ####
+    s3[E].reorder(E.axis[1], E.axis[0])
+    s3[AB].compute_at(s3[ABC], ABC.axis[0])
 
     return (
         hcl.build(s1, target=target),
@@ -93,10 +73,15 @@ def main(P=16, Q=22, R=18, S=24, alpha=0.1, beta=0.1):
     f2(A, B, C, D, res2)
     f3(A, B, C, D, res3)
     golden = alpha * np.matmul(np.matmul(A, B), C) + beta * D
-    if np.allclose(golden, res1) and np.allclose(res1, res2) and np.allclose(res2, res3):
+    if (
+        np.allclose(golden, res1)
+        and np.allclose(res1, res2)
+        and np.allclose(res2, res3)
+    ):
         print("passed")
     else:
         print("failed")
+
 
 if __name__ == "__main__":
     main()

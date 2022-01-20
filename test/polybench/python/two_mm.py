728973e581d5b8ed9b699bb27c054fcd84bcb7bc
Niansong Zhang <nz264@cornell.edu>
2022-01-20 14:09:12 -0500

    [Test] two_mm passed

--- test/polybench/python/two_mm.py
+++ test/polybench/python/two_mm.py
@@ -41,21 +41,21 @@ def top_2mm(
 
     #### Applying customizations ####
 
-    AB = kernel_2mm.out_AB
-    ABC = kernel_2mm.out_ABC
-    E = kernel_2mm.E
+    # AB = kernel_2mm.out_AB
+    # ABC = kernel_2mm.out_ABC
+    # E = kernel_2mm.E
 
-    ## N Buggy 1
-    s1[AB].compute_at(s1[ABC], ABC.axis[0])
-    s1[ABC].compute_at(s1[E], E.axis[0])
+    # ## N Buggy 1
+    # s1[AB].compute_at(s1[ABC], ABC.axis[0])
+    # s1[ABC].compute_at(s1[E], E.axis[0])
 
-    ## N Buggy 2
-    s2[AB].compute_at(s2[ABC], ABC.axis[0])
-    s2[ABC].compute_at(s2[E], E.axis[1])
+    # ## N Buggy 2
+    # s2[AB].compute_at(s2[ABC], ABC.axis[0])
+    # s2[ABC].compute_at(s2[E], E.axis[1])
 
-    ## N Buggy 3
-    s3[E].reorder(E.axis[1], E.axis[0])
-    s3[AB].compute_at(s3[ABC], ABC.axis[0])
+    # ## N Buggy 3
+    # s3[E].reorder(E.axis[1], E.axis[0])
+    # s3[AB].compute_at(s3[ABC], ABC.axis[0])
 
     ## Buggy 1
     # s[AB].compute_at(s[ABC], ABC.axis[1])
@@ -80,19 +80,20 @@ def top_2mm(
     )
 
 
-def main(P=16, Q=22, R=18, S=24):
-    f1, f2, f3 = top_2mm(P=P, Q=Q, R=R, S=S)
-    A = np.random.randint(10, (P, Q)).astype(np.float32)
-    B = np.random.randint(10, (Q, R)).astype(np.float32)
-    C = np.random.randint(10, (R, S)).astype(np.float32)
-    D = np.random.randint(10, (P, S)).astype(np.float32)
+def main(P=16, Q=22, R=18, S=24, alpha=0.1, beta=0.1):
+    f1, f2, f3 = top_2mm(P=P, Q=Q, R=R, S=S, alpha=alpha, beta=beta)
+    A = np.random.randint(10, size=(P, Q)).astype(np.float32)
+    B = np.random.randint(10, size=(Q, R)).astype(np.float32)
+    C = np.random.randint(10, size=(R, S)).astype(np.float32)
+    D = np.random.randint(10, size=(P, S)).astype(np.float32)
     res1 = np.zeros((P, S), dtype=np.float32)
     res2 = np.zeros((P, S), dtype=np.float32)
     res3 = np.zeros((P, S), dtype=np.float32)
     f1(A, B, C, D, res1)
     f2(A, B, C, D, res2)
     f3(A, B, C, D, res3)
-    if np.allclose(res1, res2) and np.allclose(res2, res3):
+    golden = alpha * np.matmul(np.matmul(A, B), C) + beta * D
+    if np.allclose(golden, res1) and np.allclose(res1, res2) and np.allclose(res2, res3):
         print("passed")
     else:
         print("failed")

621467708bda98e3b5742a6c629a08950a812c83
Niansong Zhang <nz264@cornell.edu>
2022-02-26 04:30:55 -0500

    [Pass] FixedPoinToInteger add func signature update

--- lib/Transforms/FixedPointToInteger.cpp
+++ lib/Transforms/FixedPointToInteger.cpp
@@ -10,8 +10,11 @@
 #include "hcl/Dialect/HeteroCLTypes.h"
 #include "hcl/Transforms/Passes.h"
 
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Affine/Utils.h"
 #include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
-
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/MemRef/Transforms/Passes.h"
 
 using namespace mlir;
 using namespace hcl;
@@ -19,17 +22,75 @@ using namespace hcl;
 namespace mlir {
 namespace hcl {
 
+// function calls also need to be handled
+
+// We also need to replace block args
+void updateFunctionSignature(FuncOp &funcOp) {
+  FunctionType functionType = funcOp.getType();
+  SmallVector<Type, 4> result_types =
+      llvm::to_vector<4>(functionType.getResults());
+  SmallVector<Type, 8> arg_types;
+  for (const auto &argEn : llvm::enumerate(funcOp.getArguments()))
+    arg_types.push_back(argEn.value().getType());
+
+  SmallVector<Type, 4> new_result_types;
+  SmallVector<Type, 8> new_arg_types;
+
+  for (Type t : result_types) {
+    if (MemRefType memrefType = t.dyn_cast<MemRefType>()) {
+      Type et = memrefType.getElementType();
+      size_t width;
+      if (FixedType ft = et.cast<FixedType>()) {
+        width = ft.getWidth();
+      } else {
+        UFixedType uft = et.cast<UFixedType>();
+        width = uft.getWidth();
+      }
+
+      Type newElementType = IntegerType::get(funcOp.getContext(), width);
+      new_result_types.push_back(memrefType.clone(newElementType));
+    //   llvm::outs() << memrefType.clone(newElementType) << "\n";
+    }
+  }
+
+  for (Type t : arg_types) {
+    if (MemRefType memrefType = t.dyn_cast<MemRefType>()) {
+      Type et = memrefType.getElementType();
+      size_t width;
+      if (FixedType ft = et.cast<FixedType>()) {
+        width = ft.getWidth();
+      } else {
+        UFixedType uft = et.cast<UFixedType>();
+        width = uft.getWidth();
+      }
+
+      Type newElementType = IntegerType::get(funcOp.getContext(), width);
+      new_arg_types.push_back(memrefType.clone(newElementType));
+    //   llvm::outs() << memrefType.clone(newElementType) << "\n";
+    }
+  }
+
+  FunctionType newFuncType =
+      FunctionType::get(funcOp.getContext(), new_arg_types, new_result_types);
+  funcOp.setType(newFuncType);
+}
+
+//
+void lowerAffineLoad(AffineLoadOp &op) {}
+
+// Lower a Fixed-point add op to AddIOp
+void lowerAdd(Operation &addOp) {}
 
 void lowerFixedAdd(FuncOp &f) {
   // get all fixed-point add ops
-  SmallVector<Operation*, 10> FixedAddOps;
-  f.walk([&](Operation* op) {
+  SmallVector<Operation *, 10> FixedAddOps;
+  f.walk([&](Operation *op) {
     if (auto add_op = dyn_cast<AddFixedOp>(op)) {
       FixedAddOps.push_back(op);
     }
   });
 
-  for (Operation* op : FixedAddOps) {
+  for (Operation *op : FixedAddOps) {
     // FixedAddOps are binary ops, they have two operands
     Value opr_l = op->getOperand(0);
     Value opr_r = op->getOperand(1);
@@ -58,17 +119,18 @@ void lowerFixedAdd(FuncOp &f) {
     // Check width and cast
 
     // Check frac and shift
-
-    
   }
-
 }
 
 bool applyFixedPointToInteger(ModuleOp &mod) {
 
-
   for (FuncOp func : mod.getOps<FuncOp>()) {
-    lowerFixedAdd(func);
+    // lowerFixedAdd(func);
+    updateFunctionSignature(func);
+    // for (Operation &op : func.getOps())
+    // if (auto new_op = dyn_cast<AffineLoadOp>(op)) {
+    //  lowerAffineLoad(new_op);
+    //}
   }
 
   return true;
@@ -76,16 +138,15 @@ bool applyFixedPointToInteger(ModuleOp &mod) {
 } // namespace hcl
 } // namespace mlir
 
-
 namespace {
 
-struct HCLFixedToIntegerTransformation 
-    : public FixedToIntegerBase<HCLFixedToIntegerTransformation>{
+struct HCLFixedToIntegerTransformation
+    : public FixedToIntegerBase<HCLFixedToIntegerTransformation> {
 
   void runOnOperation() override {
     auto mod = getOperation();
     if (!applyFixedPointToInteger(mod))
-      return signalPassFailure();  
+      return signalPassFailure();
   }
 };
 
@@ -96,7 +157,7 @@ namespace hcl {
 
 // Create A Fixed-Point to Integer Pass
 std::unique_ptr<OperationPass<ModuleOp>> createFixedPointToIntegerPass() {
-    return std::make_unique<HCLFixedToIntegerTransformation>();
+  return std::make_unique<HCLFixedToIntegerTransformation>();
 }
 
 } // namespace hcl

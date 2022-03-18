5b46215000ffc0ec2af513b0ef8cfa3f70f9de08
Niansong Zhang <nz264@cornell.edu>
2022-03-18 16:06:48 -0400

    [Op] Add python binding for PrintOp

--- include/hcl/Bindings/Python/hcl/build_ir.py
+++ include/hcl/Bindings/Python/hcl/build_ir.py
@@ -1097,6 +1097,11 @@ class MathExpOp(UnaryOp):
         super().__init__(math.ExpOp, F32Type.get(), val)
 
 
+class PrintOp(UnaryOp):
+    def __init__(self, val, dtype):
+        super().__init__(hcl_d.PrintOp, get_mlir_type(dtype), val)
+
+
 class MathPowOp(BinaryOp):
     def __init__(self, x, y):
         if not isinstance(x, (int, float)):

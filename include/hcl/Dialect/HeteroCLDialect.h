7a9c0770fd34b7e459cc3a988c5037f6e93d7f10
Niansong Zhang <nz264@cornell.edu>
2022-04-12 17:33:41 -0400

    [Type] Add struct type declaration

--- include/hcl/Dialect/HeteroCLDialect.h
+++ include/hcl/Dialect/HeteroCLDialect.h
@@ -13,4 +13,32 @@
 
 #include "hcl/Dialect/HeteroCLDialect.h.inc"
 
+namespace mlir {
+namespace hcl {
+namespace detail {
+struct StructTypeStorage;
+} // namespace detail
+} // namespace hcl
+} // namespace mlir
+
+namespace mlir {
+namespace hcl {
+class StructType : public mlir::Type::TypeBase<StructType, mlir::Type,
+                                               detail::StructTypeStorage> {
+public:
+  using Base::Base;
+
+  // Create an instance of a `StructType` with the given element types.
+  // There must be at least one element type.
+  static StructType get(ArrayRef<mlir::Type> elementTypes);
+
+  /// Return the element types of this struct type.
+  ArrayRef<mlir::Type> getElementTypes() const;
+
+  /// Return the number of elements in this struct type.
+  unsigned getNumElements() const { return getElementTypes().size(); };
+};
+} // namespace hcl
+} // namespace mlir
+
 #endif // HETEROCL_DIALECT_H

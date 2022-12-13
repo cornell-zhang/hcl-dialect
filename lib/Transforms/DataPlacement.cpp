d3fb5a9ed90e2025b132509f3ddd13e22189b51a
Niansong Zhang <nz264@cornell.edu>
2022-12-12 23:52:23 -0500

    [Transform][DataPlace] Add DataFlowGraph class

--- lib/Transforms/DataPlacement.cpp
+++ lib/Transforms/DataPlacement.cpp
@@ -4,6 +4,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <map>
+
 #include "PassDetail.h"
 
 #include "hcl/Dialect/HeteroCLDialect.h"
@@ -20,9 +22,40 @@ using namespace hcl;
 namespace mlir {
 namespace hcl {
 
+class Node {
+  // Member variables
+  Operation *op;
+  int device;
+  std::vector<Node *> upstream;
+  std::vector<Node *> downstream;
+ public:
+  Node(Operation *op) : op(op) {}
+  void addUpstream(Node *node) { upstream.push_back(node); }
+  void addDownstream(Node *node) { downstream.push_back(node); } 
+};
+
+class DataFlowGraph {
+  // Member variables
+  std::vector<Node *> nodes;
+ public:
+  void addNode(Node *node) { nodes.push_back(node); }
+  void addEdge(Node *src, Node *dst) {
+    src->addDownstream(dst);
+    dst->addUpstream(src);
+  }
+};
+
 
 /// Pass entry point
-bool applyDataPlacement(ModuleOp &module) {
+bool applyDataPlacement(ModuleOp &module) { 
+  // try creating a new module
+  OpBuilder builder(module.getContext());
+  builder.setInsertionPointToStart(module.getBody());
+  builder.create<ModuleOp>(module.getLoc());
+//   auto newModule = ModuleOp::create(module.getLoc());
+  // move all ops in the old module to the new module
+//   newModule.getBody()->getOperations().splice(
+//         newModule.getBody()->begin(), module.getBody()->getOperations());
   return true;
 }
 

3eae1ef0719e9aff82b1baf2ad1605c861eda3e5
chhzh123 <hc676@cornell.edu>
2023-02-25 16:27:36 -0500

    Add formatting script

---
+++ .circleci/task_format.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright HeteroCL authors. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+set -u
+set -o pipefail
+
+echo "Check license header..."
+python3 scripts/check_license_header.py HEAD~1
+
+echo "Check C/C++ formats using clang-format..."
+bash ./scripts/git-clang-format.sh HEAD~1
